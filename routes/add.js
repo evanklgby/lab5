5f88d2247d83f3e757bfe752c331ff70f24ccbe9
evanklgby <eklam@ucsd.edu>
2021-02-04 01:38:20 -0800

    lab 5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,12 @@
 var data = require("../data.json");
 
-exports.addFriend = function(request, response) {    
-	// Your code goes here
- }
+exports.addFriend = function(request, response) { 
+  var myFriend = {
+      "name": request.query.name,
+      "description": request.query.description,
+      "imageURL": "https://picsum.photos/536/354"
+   };
+  console.log(myFriend);
+  data.friends.push(myFriend);
+  response.render('index', data);
+}
